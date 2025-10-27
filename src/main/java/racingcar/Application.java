dd2df1efd599801cf0ad49476ab0cd558fac229b
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-10-27 20:08:21 +0900

    feat(app): add Application class with main method to start the game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.service.CarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        CarService carService = new CarService();
+        OutputView outputView = new OutputView();
+
+        CarController carController = new CarController(inputView, outputView, carService);
+        carController.run();
     }
 }
