dc390648f14b529e9b1097f2c56a4bd97099a9e3
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-10-27 20:07:40 +0900

    feat(view): implement InputView and OutputView for user interaction

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,29 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.service.Validator;
+
+import java.util.ArrayList;
+
+public class InputView {
+
+    public ArrayList<String> returnCarname() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+
+        ArrayList<String> names = new ArrayList<>();
+        String[] tokens = carNames.split(",", -1);
+        for (String token : tokens) {
+            names.add(token.trim());
+        }
+
+        Validator.validateCarNames(names);
+        return names;
+    }
+
+    public int returnTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String strInput = Console.readLine();
+        return Validator.parseAndValidateTryCount(strInput);
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,23 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void returnResult(List<Car> cars) {
+        for (Car car : cars) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < car.getMoveCount(); i++) {
+                sb.append("-");
+            }
+            System.out.println(car.getName() + " : " + sb.toString());
+        }
+        System.out.println();
+    }
+
+    public void returnWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
