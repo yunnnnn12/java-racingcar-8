c53b14202db797232fdecd184c580464613a17d9
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-10-27 20:08:06 +0900

    feat(controller): implement CarController to orchestrate game flow

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,2 +1,33 @@
-package racingcar.controller;public class CarController {
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.service.CarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class CarController {
+
+    private InputView inputView;
+    private CarService carService;
+    private OutputView outputView;
+
+    public CarController(InputView inputView, OutputView outputView, CarService carService) {
+        this.inputView = inputView;
+        this.carService = carService;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<String> carNames = inputView.returnCarname();
+        int tryCount = inputView.returnTryCount();
+
+        List<Car> cars = carService.createCars(carNames);
+        carService.race(cars, tryCount);
+        outputView.returnResult(cars);
+
+        List<String> winners = carService.selectWinners(cars);
+        outputView.returnWinners(winners);
+    }
 }
